deb497f14b63ff3b3b25e33aaabc34ceffdaf424
me <jbindas@costco.com>
2019-09-04 21:46:12 +0000

    more stuff

--- main.tf
+++ main.tf
@@ -2,3 +2,17 @@ resource "azurerm_resource_group" "myresourcegroup" {
   name     = "${var.prefix}-workshop"
   location = "${var.location}"
 }
+
+module "web_app_container" {
+  source              = "app.terraform.io/jbindas-sandbox/web-app-container/azurerm"
+  name                = "${var.prefix}"
+  port                = "80"
+  https_only          = "false"
+  resource_group_name = "${azurerm_resource_group.myresourcegroup.name}"
+  container_type      = "docker"
+  container_image     = "scarolan/palacearcade"
+}
+
+output "container_app_url" {
+  value = "http://${module.web_app_container.hostname}"
+}
