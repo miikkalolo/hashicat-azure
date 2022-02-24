dcb207a18f924eba6fa727193a37e66f3b05d685
miikkalolo <54904210+miikkalolo@users.noreply.github.com>
2022-02-24 10:35:26 -0800

    stage changes

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Wag More!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own. 
+  Welcome to ${PREFIX}'s app. What have I done. Opps. 
   <!-- END -->
   
   </div>
